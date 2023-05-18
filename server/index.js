7b27258b3d789859e44f35613aae01b4bfa28343
Harry Lee <footok@gmail.com>
2023-05-18 22:13:41 +1000

    remove unused code

--- server/index.js
+++ server/index.js
@@ -10,19 +10,17 @@ const __dirname = dirname(__filename);
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-app.use('/cats', catRouter);
 
 // Have Node serve the files for our built React app
 app.use(express.static(resolve(__dirname, '../client/build')));
 
-app.get("/api", (req, res) => {
-  res.json({ message: "Hello from CatWiki!" });
-});
+app.use('/cats', catRouter);
 
 // All other GET requests not handled before will return our React app
 app.get('*', (req, res) => {
   res.sendFile(resolve(__dirname, '../client/build', 'index.html'));
 });
+
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
