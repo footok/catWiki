6a042c9cb91d7f6ccd3ce7e3b5ee02763b0aaf05
Harry Lee <footok@gmail.com>
2023-05-18 23:39:21 +1000

    create a route to get images by breed id

--- server/controllers/cats.js
+++ server/controllers/cats.js
@@ -9,3 +9,15 @@ export const getBreeds = async (req, res) => {
     res.status(500).json({ error: 'An error occurred while fetching cat image' });
   }
 }
+
+export const getBreedImagesById = async (req, res) => {
+  try {
+    const url = `https://api.thecatapi.com/v1/images/search?limit=10&breed_ids=${req.params.id}`
+    const response = await axios.get(url)
+    console.log('response:', response)
+    res.json(response.data);
+  } catch (error) {
+    console.error('An error occurred while fetching cat image:', error);
+    res.status(500).json({ error: 'An error occurred while fetching cat image' });
+  }
+}
--- server/routes/cats.js
+++ server/routes/cats.js
@@ -1,9 +1,10 @@
 import express from 'express';
 
-import { getBreeds } from '../controllers/cats.js'
+import { getBreeds, getBreedImagesById } from '../controllers/cats.js'
 
 const router = express.Router();
 
 router.get('/breeds', getBreeds);
+router.get('/breed/:id/images', getBreedImagesById);
 
 export default router;
