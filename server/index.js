4292424ad31f13ec916f0b8ccc48df82cfb70ef3
Harry Lee <footok@gmail.com>
2023-05-18 12:07:30 +1000

    update server index to use es6 style and update package.json

--- server/index.js
+++ server/index.js
@@ -1,12 +1,15 @@
-const path = require('path');
-const express = require("express");
+import express from 'express';
+import { fileURLToPath } from 'url';
+import { dirname, resolve } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const PORT = process.env.PORT || 3001;
-
 const app = express();
 
 // Have Node serve the files for our built React app
-app.use(express.static(path.resolve(__dirname, '../client/build')));
+app.use(express.static(resolve(__dirname, '../client/build')));
 
 app.get("/api", (req, res) => {
   res.json({ message: "Hello from CatWiki!" });
@@ -14,7 +17,7 @@ app.get("/api", (req, res) => {
 
 // All other GET requests not handled before will return our React app
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+  res.sendFile(resolve(__dirname, '../client/build', 'index.html'));
 });
 
 app.listen(PORT, () => {
