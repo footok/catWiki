858b5c806c87720c3fb14c3e0561f6c7cba9f920
Harry Lee <footok@gmail.com>
2023-05-18 22:20:12 +1000

    fetch breeds from the api and add option to react-select

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,11 @@
 import React from "react";
-import logo from "./logo.svg";
 import { Navbar } from "./components";
 import "./App.css";
 
 function App() {
-  const [data, setData] = React.useState(null);
-
-  React.useEffect(() => {
-    fetch("/api")
-      .then((res) => res.json())
-      .then((data) => setData(data.message));
-  }, []);
-
   return (
     <div className="App">
       <Navbar />
-      <header className="App-header">
-        <p>{!data ? "Loading..." : data}</p>
-      </header>
     </div>
   );
 }
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,16 +1,49 @@
+import { useState, useEffect } from "react";
+import Select from 'react-select';
+
+import * as api from '../api'
 import "../styles/Navbar.css";
 import logo from "../images/logo128.png";
 
-const Navbar = () => (
-  <nav className="nav-container">
-    <div className="logo-container">
-      <img className="nav-logo" src={logo} alt="logo" />
-    </div>
-    <div className="search-bar-container">
-      <input className="search-bar" placeholder="Search cat breeds here.." />
-      <button className="btn" type="button">Search</button>
-    </div>
-  </nav>
-)
+const Navbar = () => {
+  const [searchQuery, setSearchQuery] = useState('');
+  const [breedOptions, setBreedOptions] = useState([{value: '', label: ''}]);
+
+  const handleSubmit = async () => {
+  }
+
+  useEffect(() => {
+    const fetchBreeds = async () => {
+      try {
+        const response = await api.fetchBreeds();
+        const breedOptions = response.data.map(breed => (
+          {
+            value: breed.id,
+            label: breed.name
+          }
+        ))
+        setBreedOptions(breedOptions);
+      } catch (error) {
+        console.error('An error occurred while fetching data:', error);
+      }
+    };
+
+    fetchBreeds();
+  }, []);
+
+  return (
+    <nav className="nav-container">
+      <div className="logo-container">
+        <img className="nav-logo" src={logo} alt="logo" />
+      </div>
+      <div className="search-bar-container">
+        <Select 
+          className="search-bar" 
+          options={breedOptions} 
+        />
+      </div>
+    </nav>
+  )
+}
 
 export default Navbar;
